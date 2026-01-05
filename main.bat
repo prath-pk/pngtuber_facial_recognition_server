3e53992ed9a67e765b45533f89adac4d5c1a3435
prath-pk <prathameshk1297@gmail.com>
2026-01-05 08:53:53 +0530

    Added readme and updated folder structure and added bat files

---
+++ main.bat
@@ -0,0 +1,6 @@
+@echo off
+title Server Starting...
+
+start /b run_py.bat
+start /b run_node.bat
+rem pause
---
+++ run_py.bat
@@ -0,0 +1,5 @@
+@echo off
+title Face Detect Starting...
+
+REM .face_detect is the virtual env I have created. You can create your own.
+.face_detect\Scripts\python.exe main.py
